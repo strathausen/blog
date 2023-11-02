64b5c0d4dc82b56273b4a8c20be6b55ce0b18c27
Philipp Strathausen <philipp@herjuno.com>
2023-11-02 21:54:02 +0100

    Revert "use vercel"
    This reverts commit 7905c5c788a2dedb524a4c983ae0746739e123aa.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -34,6 +33,4 @@ export default defineConfig({
     "/wunderkarte": "/archive/wunderkarte",
     "/heroku-awk-pipes-in-colour": "/archive/heroku-awk-pipes-in-colour",
   },
-  output: 'server',
-  adapter: vercel(),
 });
