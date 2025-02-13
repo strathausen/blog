0e871b28d29d5734f423906dfb6f434ad21566a3
Philipp Strathausen <strathausen@gmail.com>
2025-02-14 00:12:58 +0100

    downgrade vercel astro + embed analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
